90361321ce34f1706af0a4dc06b64138802da476
Raphael Cerqueira <raphaelfc@gmail.com>
2021-11-16 19:00:15 -0300

    Initial Commit

---
+++ OpenSeaTabBar/OpenSeaTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenSeaTabBarApp.swift
+//  OpenSeaTabBar
+//
+//  Created by Raphael Cerqueira on 16/11/21.
+//
+
+import SwiftUI
+
+@main
+struct OpenSeaTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
